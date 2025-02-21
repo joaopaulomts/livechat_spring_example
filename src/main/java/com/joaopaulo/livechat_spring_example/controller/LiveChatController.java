c99dea11eb8fa5f39798f51c6b8e7eb98c915800
joaopaulomts <jp.nmts@gmail.com>
2025-02-21 00:34:23 -0300

    refactor: ajustando response no chat

--- src/main/java/com/joaopaulo/livechat_spring_example/controller/LiveChatController.java
+++ src/main/java/com/joaopaulo/livechat_spring_example/controller/LiveChatController.java
@@ -2,16 +2,18 @@ package com.joaopaulo.livechat_spring_example.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
 import com.joaopaulo.livechat_spring_example.domain.ChatInput;
 import com.joaopaulo.livechat_spring_example.domain.ChatOutput;
 
+@Controller
 public class LiveChatController {
 
     @MessageMapping("/new-message")
     @SendTo("/topics/livechat")
     public ChatOutput newMessage(ChatInput input) {
-        return new ChatOutput(HtmlUtils.htmlEscape(input.user() + ":" + input.message()));
+        return new ChatOutput(HtmlUtils.htmlEscape(input.user() + ": " + input.message()));
     }
 }
